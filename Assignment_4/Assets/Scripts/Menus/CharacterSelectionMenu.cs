8011489e1280a6b9b5840e5304a6fbb6f7e531e1
TheJewGamer <jcohen0604@gmail.com>
2020-02-12 14:11:37 -0600

    Assignment 4 script updates

--- Assignment_4/Assets/Scripts/Menus/CharacterSelectionMenu.cs
+++ Assignment_4/Assets/Scripts/Menus/CharacterSelectionMenu.cs
@@ -8,6 +8,7 @@ public class CharacterSelectionMenu : MonoBehaviour
     //variables
     public GameObject menu;
     public Character character;
+    private bool rangerClassSelected = false;
 
     //elements
     public Text healthText;
@@ -17,46 +18,101 @@ public class CharacterSelectionMenu : MonoBehaviour
     //buttons
     public Button KnifeButton;
     public Button RifleButton;
+    public Button SpyButton;
+    public Button RangerButton;
 
     public void Start()
     {
         //make sure select class first
         KnifeButton.interactable = false;
         RifleButton.interactable = false;
+
+        //set color
+        KnifeButton.GetComponent<Image>().color = Color.red;
+        RifleButton.GetComponent<Image>().color = Color.red;
     }
 
     //ranger class selected
     public void SetClassRanger()
     {
+        //update
         this.character = new ClassRanger();
+        rangerClassSelected = true;
         UpdateStats();
+
+        //feedback
+        RangerButton.GetComponent<Image>().color = Color.green;
+        SpyButton.GetComponent<Image>().color = Color.grey;
 
         //enable weapon selection 
         KnifeButton.interactable = true;
         RifleButton.interactable = true;
+        KnifeButton.GetComponent<Image>().color = Color.grey;
+        RifleButton.GetComponent<Image>().color = Color.grey;
     }
 
     //spy class selected
     public void SetClassSpy()
     {
         this.character = new ClassSpy();
+        rangerClassSelected = false;
         UpdateStats();
+
+        //feedback
+        RangerButton.GetComponent<Image>().color = Color.grey;
+        SpyButton.GetComponent<Image>().color = Color.green;
 
         //enable weapon selection 
         KnifeButton.interactable = true;
         RifleButton.interactable = true;
+        KnifeButton.GetComponent<Image>().color = Color.grey;
+        RifleButton.GetComponent<Image>().color = Color.grey;
     }
 
     public void SetWeaponRifle()
     {
+        if(rangerClassSelected)
+        {
+            this.character = new ClassRanger();
+        }
+        else
+        {
+            this.character = new ClassSpy();
+        }
+
         this.character = new WeaponRifle(character);
         UpdateStats();
+
+        //feedback
+        RifleButton.GetComponent<Image>().color = Color.green;
+        KnifeButton.GetComponent<Image>().color = Color.grey;
+
+        //prevent clicking
+        KnifeButton.interactable = true;
+        RifleButton.interactable = false;
     }
 
     public void SetWeaponKnife()
     {
+        if (rangerClassSelected)
+        {
+            this.character = new ClassRanger();
+        }
+        else
+        {
+            this.character = new ClassSpy();
+        }
+
         this.character = new WeaponKnife(character);
         UpdateStats();
+
+        //feedback
+        RifleButton.GetComponent<Image>().color = Color.grey;
+        KnifeButton.GetComponent<Image>().color = Color.green;
+
+        //prevent clicking
+        KnifeButton.interactable = false;
+        RifleButton.interactable = true;
     }
 
     private void UpdateStats()
