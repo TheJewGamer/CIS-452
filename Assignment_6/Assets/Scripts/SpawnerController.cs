8f79a96098e6b2638fba45795f2b03a3dd68fa54
TheJewGamer <jcohen0604@gmail.com>
2020-02-25 17:02:55 -0600

    script updates

--- Assignment_6/Assets/Scripts/SpawnerController.cs
+++ Assignment_6/Assets/Scripts/SpawnerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class SpawnerController : MonoBehaviour
 {
+    //vars
+    private float spawnDelay;
+    private List<Transform> spawners = new List<Transform>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //get spawners
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag("spawnpoint"))
+        {
+            spawners.Add(item.transform);
+        }
+
+        //loop spawn
+        InvokeRepeating("SpawnEnemy", spawnDelay, spawnDelay);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnEnemy()
     {
-        
+        //get spawn point
+        int spawnIndex = Random.Range(0, spawners.Count);
+
+        //get badguy type
+        string badGuyType = badGuyTypes[Random.Range(0, badGuyTypes.Length)];
+
+        //spawn
+        badGuyCurrentSpawn = Instantiate(badGuyPreFab, spawners[spawnIndex].position, spawners[spawnIndex].rotation);
+
+        //call and set bad guy type
+        badGuyCurrentSpawn = factory.CreateBadGuy(badGuyType, badGuyCurrentSpawn);
     }
+
+    private void SpawnFriendly()
+    {
+
+    }
+
 }
