b5c1da56b9c6dc141c0d128afdbba5ad5f93b514
Jacob <jcohen0604@gmail.com>
2020-04-07 17:20:36 -0500

    set up

---
+++ Assignment_10/Assets/Scripts/WinLoseMenuController.cs
@@ -0,0 +1,27 @@
+﻿/*
+    * Jacob Cohen
+    * WinLoseMenuController.cs
+    * Assignment #10
+    * controls the win and lose menu
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLoseMenuController : MonoBehaviour
+{
+    public void RetryPushed()
+    {
+        Time.timeScale = 0;
+
+        SceneManager.LoadScene("Game");
+    }
+
+    public void MainMenuPushed()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
