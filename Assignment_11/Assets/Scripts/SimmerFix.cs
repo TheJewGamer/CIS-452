002cb65a883f9f433ea28415109e4843cfac04e8
Jacob <jcohen0604@gmail.com>
2020-04-17 14:16:36 -0500

    Simmer Fix

---
+++ Assignment_11/Assets/Scripts/SimmerFix.cs
@@ -0,0 +1,33 @@
+﻿/*
+    * Jacob Cohen
+    * SimmerFix.cs
+    * Assignment #11
+    * makes the camera work on build
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class SimmerFix : MonoBehaviour {
+
+    // Set this to the in-world distance between the left & right edges of your scene.
+    public float sceneWidth = 12;
+
+    Camera _camera;
+    void Start() {
+        _camera = GetComponent<Camera>();
+    }
+
+    // Adjust the camera's height so the desired scene width fits in view
+    // even if the screen/window size changes dynamically.
+    void Update() {
+        float unitsPerPixel = sceneWidth / Screen.width;
+
+        float desiredHalfHeight = 0.5f * unitsPerPixel * Screen.height;
+
+        _camera.orthographicSize = desiredHalfHeight;
+    }
+}
